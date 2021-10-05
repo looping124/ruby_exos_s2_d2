066d80753c284cc34e8e27b64e0666531206c21e
Cedric Le Net <lenetcedric@gmail.com>
2021-10-05 09:22:11 +0200

    final2

--- exo_15.rb
+++ exo_15.rb
@@ -1,6 +1,11 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-print ">"
+print "Entre un nombre entre 1 et 25 >"
 nombre = gets.chomp.to_i
+
+while nombre > 25 || nombre < 1
+  print "Entre un nombre entre 1 et 25 >"
+  nombre = gets.chomp.to_i
+end
 
 nombre.times do |i|
   j=i+1
